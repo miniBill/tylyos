b3cac6bac2c8d2c6206f16d93c64042e5965b00a
minibill <minibill@f256b4f0-ec0d-4cd5-9998-35f0922a32a5>
2008-05-10 18:28:33 +0000

    You don't need to use grub anymore
    git-svn-id: svn+ssh://svn.gna.org/svn/clearos/trunk@9 f256b4f0-ec0d-4cd5-9998-35f0922a32a5

--- make.sh
+++ make.sh
@@ -1,6 +1,12 @@
 if [[ -e floppy.img ]]; then
 	su -c "sh make.su"
 else
-	su -c "sh remake.su"
+	if [[ -e init.img.bz2 ]]; then
+		cp init.img.bz2 floppy.img.bz2
+		bunzip2 floppy.img.bz2
+		su -c "sh make.su"
+	else
+		su -c "sh remake.su"
+	fi
 fi
 
