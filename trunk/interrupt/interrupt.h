123e72e577e0ff54040523d7f708e24ebad4220c
minibill <minibill@f256b4f0-ec0d-4cd5-9998-35f0922a32a5>
2008-05-15 16:38:41 +0000

    IDT: #1
    git-svn-id: svn+ssh://svn.gna.org/svn/clearos@10 f256b4f0-ec0d-4cd5-9998-35f0922a32a5

--- trunk/interrupt/interrupt.h
+++ trunk/interrupt/interrupt.h
@@ -35,32 +35,58 @@ struct idt_ptr{
 struct idt_entry idt[256];
 struct idt_ptr idt_pointer;
 
+
 extern void idt_load();
 
-/*void add_IDTseg(short int i, void (*gestore)(), unsigned char options, unsigned int seg_sel);*/
+void initIDT();
 
-/*void set_idtr(IDT_Descriptor *addr, unsigned short int limit);*/
+void addIDTseg(short int i, void (*gestore)(), unsigned char options, unsigned int seg_sel);
 
+
+
+/*
+	rimappa i PIC (programmable input controller)
+	offset_1: offset riferito alla IDT delle interruzioni per il primo PIC
+	offset_2: offset riferito alla IDT delle interruzioni per il PIC slave
+*/
+void irq_remap (unsigned int offset_1, unsigned int offset_2);
+
+void interrupt_handler (unsigned int eax,
+                        unsigned int ecx,
+                        unsigned int edx,
+                        unsigned int ebx,
+                        unsigned int ebp,
+                        unsigned int esi,
+                        unsigned int edi,
+                        unsigned int ds,
+                        unsigned int es,
+                        unsigned int fs,
+                        unsigned int gs,
+                        unsigned int isr,
+                        unsigned int error,
+                        unsigned int eip,
+                        unsigned int cs,
+                        unsigned int eflags, ...);
 /*############### funzioni interruzioni ################*/
-void int0();
-void int1();
-void int2();
-void int3();
-void int4();
-void int5();
-void int6();
-void int7();
-void int8();
-void int9();
-void int10();
-void int11();
-void int12();
-void int13();
-void int14();
-void int15();
-void int16();
-void int17();
-void int18();
-void int19();
+extern void isr_0();
+extern void isr_1();
+extern void isr_2();
+extern void isr_3();
+extern void isr_4();
+extern void isr_5();
+extern void isr_6();
+extern void isr_7();
+extern void isr_8();
+extern void isr_9();
+extern void isr_10();
+extern void isr_11();
+extern void isr_12();
+extern void isr_13();
+extern void isr_14();
+extern void isr_15();
+extern void isr_16();
+extern void isr_17();
+extern void isr_18();
+extern void isr_32();
 
 #endif
--- trunk/kernel/stdio.h
+++ trunk/kernel/stdio.h
@@ -4,5 +4,9 @@
 #define MAXN 32 /*lunghezza massima in cifre di un numero*/
 
 int strapp(char* dest,char* format,void* p);
+
+unsigned char inb(int portnum);
+void outb(int data, int portnum);
+
 #endif
 
