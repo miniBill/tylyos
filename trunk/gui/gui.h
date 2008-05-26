e728df273cc92b1be93dee46f96ccd4c8aace61e
morgoth90 <morgoth90@f256b4f0-ec0d-4cd5-9998-35f0922a32a5>
2008-05-26 16:36:05 +0000

    funzioni grafiche di base
    git-svn-id: svn+ssh://svn.gna.org/svn/clearos@54 f256b4f0-ec0d-4cd5-9998-35f0922a32a5

---
+++ trunk/gui/gui.h
@@ -0,0 +1,31 @@
+/* Copyright (C) 2008 Leonardo Taglialegne <leonardotaglialegne+clearos@gmail.com>
+ * Copyright (C) 2008 Luca Salmin
+ *
+ * This file is part of ClearOS.
+ *
+ * ClearOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ClearOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ClearOS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../drivers/screen/screen.h"
+
+#define BORDER_ORIZONTAL 205
+#define BORDER_VERTICAL 186
+#define BORDER_CORNER_HI_LEFT 201
+#define BORDER_CORNER_HI_RIGHT 187
+#define BORDER_CORNER_LOW_LEFT 200
+#define BORDER_CORNER_LOW_RIGHT 188
+
+void DrawRectangle(int x,int y,int width,int height);
+void DrawRectangleExt(int x,int y,int width,int height,char color);
+
