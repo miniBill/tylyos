c31ccce4465e8e6120c3dddd6d067aabd0c28bfc
minibill <minibill@f256b4f0-ec0d-4cd5-9998-35f0922a32a5>
2008-06-13 06:19:37 +0000

    Numlock, semi-working.
    git-svn-id: svn+ssh://svn.gna.org/svn/clearos@111 f256b4f0-ec0d-4cd5-9998-35f0922a32a5

--- trunk/kernel/kernel.c
+++ trunk/kernel/kernel.c
@@ -338,7 +338,7 @@ void _kmain(multiboot_info_t* mbd, unsigned int magic){
     gotoxy(1,t+1);
     asm("sti");
     setCursorPos(79,24);
-    writexy(0,ROWS-1,"[s][c][a] Time:");
+    writexy(0,ROWS-1,"[s][c][a][n] Time:");
     on=1;
     while(on);
     {
