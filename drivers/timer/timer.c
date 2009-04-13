195bbbc6a62763d6cbae56d10e9dc5a8cd4827ac
morgoth90 <morgoth90@f256b4f0-ec0d-4cd5-9998-35f0922a32a5>
2009-04-13 15:08:30 +0000

    tick del timer sistemato (1 tick ogni millisecondo)
    git-svn-id: svn+ssh://svn.gna.org/svn/clearos/trunk@174 f256b4f0-ec0d-4cd5-9998-35f0922a32a5

--- drivers/timer/timer.c
+++ drivers/timer/timer.c
@@ -8,29 +8,25 @@ static unsigned long int timeCount=0;
 
 void initTimer()
 {
-    unsigned int divisor=1000;/*TODO:ricontrollare l'inizializzazione in modo da garantire tick ad intervalli conosciuti*/
+    unsigned int divisor=1193;/*interrupt ogni 1ms*/
     asm("cli");
     timeCount=0;
-    outb(PIT_COMMREG,0x34);
+    outb(PIT_COMMREG,0x36);
     outb(PIT_DATAREG0,divisor & 0xFF);
     outb(PIT_DATAREG0,divisor >> 8);
     asm("sti");
 }
 
 void tick(void){
-    static short ticks=0;
-    ticks++;
-    if(ticks==0x40){
-        char timestring[9]={0};
-        int print=COLUMNS;
-        ticks=0;
-        timeCount++;
-        if(timeCount==3600)
-            timeCount=0;
-        itoa(timeCount,timestring);
-        while(timestring[COLUMNS-print]!=0)
-            print--;
-        writexy(print,ROWS-1,timestring);
+    timeCount++;
+    if(timeCount%100==0)
+    {
+    char timestring[9]={0};
+    int print=COLUMNS;
+    itoa(timeCount,timestring);
+    while(timestring[COLUMNS-print]!=0)
+        print--;
+    writexy(print,ROWS-1,timestring);
     }
 }
 
