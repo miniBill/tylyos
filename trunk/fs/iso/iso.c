a3d9a80e3d504b7d16246b633a360cb0d9546122
minibill <minibill@f256b4f0-ec0d-4cd5-9998-35f0922a32a5>
2009-04-17 15:21:22 +0000

    Non va cmq, ma perlomeno DOVREBBE...
    git-svn-id: svn+ssh://svn.gna.org/svn/clearos@189 f256b4f0-ec0d-4cd5-9998-35f0922a32a5

--- trunk/fs/iso/iso.c
+++ trunk/fs/iso/iso.c
@@ -10,7 +10,7 @@ void test_iso(void){
 		/*mi interessa il 16 settore, per settori da 2048 byte, e per "blocchi" da 256 byte*/
 		int sector=2048*15/256;
 		unsigned char buffer[256];
-		printf("Testing %x:%x",&controller,&hdd);
+		printf("Testing %x:%x",controller,hdd);
 		readSector(controller,hdd,sector,buffer);
 		printf("%c%c%c%c%c%c\n",&buffer[0],&buffer[1],&buffer[2],&buffer[3],&buffer[4],&buffer[5]);
 	}
