69806ce29a59780575832373390fdcd2dab6f023
minibill <minibill@f256b4f0-ec0d-4cd5-9998-35f0922a32a5>
2009-09-06 09:36:16 +0000

    Hdd test moved
    git-svn-id: svn+ssh://svn.gna.org/svn/clearos@201 f256b4f0-ec0d-4cd5-9998-35f0922a32a5

---
+++ trunk/kernel/hddtest.c
@@ -0,0 +1,64 @@
+/* Copyright (C) 2008 Leonardo Taglialegne <leonardotaglialegne+clearos@gmail.com>
+ *
+ * This file is part of ClearOS.
+ *
+ * ClearOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ClearOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ClearOS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "kernel.h"
+#include <drivers/hdd/ata.h>
+#include <lib/string.h>
+
+int checkHdd(int controller,int disk){
+	int present=0;
+	int t=row()+1;
+	NO(t);
+	greendot();
+	present=isHddPresent(controller,disk);
+	printf("Rilevamento presenza   hdd: %s %s\n",disk?"slave     ":"master    ",present?"presente":"assente");
+	if(present){
+		OK(t);
+		/*t=row()+1;
+		NO(t);
+		greendot();*/
+		if(identifyHdd(controller,disk)){}
+			/*OK(t);*/
+	}
+	return present;
+}
+
+int checkController(int controller){
+	int present=0;
+	int ret=0;
+	int t=row()+1;
+	NO(t);
+	greendot();
+	present=isControllerPresent(controller);
+	printf("Rilevamento controller hdd: %s %s\n",controller?"secondario":"primario  ",present?"presente":"assente");
+	if(present)
+		OK(t);
+	else
+		return 0;
+	ret|=8;
+	ret|=checkHdd(controller,0)<<2;
+	ret|=checkHdd(controller,1)<<1;
+	return ret;
+}
+
+int checkHdds(void){
+	int ret=0;
+	ret|=checkController(0);
+	ret|=checkController(1)<<4;
+	return ret;
+}
--- trunk/kernel/kernel.c
+++ trunk/kernel/kernel.c
@@ -19,6 +19,7 @@
 
 #include "kernel.h"
 #include "tests.h"
+#include "hddtest.h"
 #include "multiboot.h"
 #include <lib/string.h>
 #include <memory/memory.h>
@@ -94,49 +95,6 @@ int check(const char * output,int offset){
             cputxy(i+3+offset,row(),Light_Red);
         }
     return retval;
-}
-
-int checkHdd(int controller,int disk){
-	int present=0;
-	int t=row()+1;
-	NO(t);
-	greendot();
-	present=isHddPresent(controller,disk);
-	printf("Rilevamento presenza   hdd: %s %s\n",disk?"slave     ":"master    ",present?"presente":"assente");
-	if(present){
-		OK(t);
-		t=row()+1;
-		NO(t);
-		greendot();
-		if(identifyHdd(controller,disk))
-			OK(t);
-	}
-	return present;
-}
-
-int checkController(int controller){
-	int present=0;
-	int ret=0;
-	int t=row()+1;
-	NO(t);
-	greendot();
-	present=isControllerPresent(controller);
-	printf("Rilevamento controller hdd: %s %s\n",controller?"secondario":"primario  ",present?"presente":"assente");
-	if(present)
-		OK(t);
-	else
-		return 0;
-	ret|=8;
-	ret|=checkHdd(controller,0)<<2;
-	ret|=checkHdd(controller,1)<<1;
-	return ret;
-}
-
-int checkHdds(void){
-	int ret=0;
-	ret|=checkController(0);
-	ret|=checkController(1)<<4;
-	return ret;
 }
 
 int magictest(void){
