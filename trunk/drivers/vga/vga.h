c2ad67e648d47b0a89f8cfd42de9e8a545a46560
morgoth90 <morgoth90@f256b4f0-ec0d-4cd5-9998-35f0922a32a5>
2010-01-12 20:28:12 +0000

    vga work in progress
    git-svn-id: svn+ssh://svn.gna.org/svn/clearos@229 f256b4f0-ec0d-4cd5-9998-35f0922a32a5

---
+++ trunk/drivers/vga/vga.h
@@ -0,0 +1,1 @@
+int init_graph_vga(int width, int height,int chain4);
--- trunk/kernel/kernel.c
+++ trunk/kernel/kernel.c
@@ -25,7 +25,7 @@
 #include <task/task.h>
 #include <interrupt/interrupt.h>
 #include <drivers/timer/timer.h>
-
+#include <drivers/vga/vga.h>
 #include <gui/gui.h>
 
 static int on=1;
@@ -234,6 +234,26 @@ void _kmain ( multiboot_info_t* mbd, unsigned int magicN )
        kmalloc(1024*1024); 
     }*/
     /*kernelPanic("_kmain()","this is a test message");*/
+init_graph_vga(320,200,0);
+{
+int x=0;
+int boh=0;
+char *addr=0xA0000;
+while(1)
+{
+	for(x=boh*2;x<320*200;x++)
+	{
+		addr[x]=(char)boh;
+	}
+	
+	boh++;
+	if(boh==256)
+		break;
+}
+
+
+
+}
     while ( on );
     {
         /*wait 3 seconds before halting*/
