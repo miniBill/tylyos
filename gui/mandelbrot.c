c86920ec5ec69292a6a9bab277c7c8377ab47a1e
minibill <minibill@f256b4f0-ec0d-4cd5-9998-35f0922a32a5>
2010-02-13 20:05:38 +0000

    Mandelbrot working
    git-svn-id: svn+ssh://svn.gna.org/svn/clearos/trunk@352 f256b4f0-ec0d-4cd5-9998-35f0922a32a5

--- gui/mandelbrot.c
+++ gui/mandelbrot.c
@@ -1,27 +1,27 @@
 #include "gui.h"
 
-static const short max=1000;
-static const int radius=100;
+static const short max=100;
+static const int radius=2;
 
 void draw_mandelbrot(void){
-  unsigned char ix,iy;
+  unsigned short ix,iy;
   unsigned short count;
   float x0,y0,x,y,tx;
   for(iy=0;iy<VGA_height;iy++)
     for(ix=0;ix<VGA_width;ix++){
       x0=ix;y0=iy;
-      x0=(x0/VGA_width)*4-2;
-      y0=(y0/VGA_height)*4-2;
-      x=y=0;
-      while(x*x+y*y < radius*radius && count<max){
+      x0=(x0/VGA_width)*2.8f-2.2f;
+      y0=(y0/VGA_height)*2.4f-1.2f;
+      x=y=count=0;
+      while(((x*x+y*y) < radius*radius) && count<max){
         tx = x*x - y*y + x0;
         y = 2*x*y + y0;
         x = tx;
         count++;
       }
       if(count==max)
-        VGA_address[VGA_width*iy+ix]=0x1;
+        VGA_address[VGA_width*iy+ix]=0x0;
       else
-        VGA_address[VGA_width*iy+ix]=0;
+        VGA_address[VGA_width*iy+ix]=(char)count;
     }
 }
