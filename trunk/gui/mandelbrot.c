29125c4e94baca883a9730b62150ab026a20aded
minibill <minibill@f256b4f0-ec0d-4cd5-9998-35f0922a32a5>
2010-02-13 20:47:26 +0000

    Colorazione
    git-svn-id: svn+ssh://svn.gna.org/svn/clearos@354 f256b4f0-ec0d-4cd5-9998-35f0922a32a5

--- trunk/gui/mandelbrot.c
+++ trunk/gui/mandelbrot.c
@@ -2,16 +2,15 @@
 
 static const int radius=2;
 
-static char convert(char count){
-  if(count<4)
-    return 0x13;
-  return count;
+static char convert(unsigned char count){
+  return count/4*32;
 }
 
 void draw_mandelbrot(void){
   unsigned short ix,iy;
   unsigned short count;
   float x0,y0,x,y,tx;
+  unsigned short min=255,max=0;
   for(iy=0;iy<VGA_height;iy++)
     for(ix=0;ix<VGA_width;ix++){
       x0=ix;y0=iy;
@@ -26,7 +25,15 @@ void draw_mandelbrot(void){
       }
       if(count==255)
         VGA_address[VGA_width*iy+ix]=0x0;
-      else
-        VGA_address[VGA_width*iy+ix]=convert(count);
+      else{
+        if(count<4)
+          VGA_address[VGA_width*iy+ix]=0x13;
+        else
+          VGA_address[VGA_width*iy+ix]=convert(count);
+        if(count<min)
+          min=count;
+        if(count>max)
+          max=count;
+      }
     }
 }
