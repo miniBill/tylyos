fc91ee20e39a076b438fb396613df63fe983879b
minibill <minibill@f256b4f0-ec0d-4cd5-9998-35f0922a32a5>
2010-02-13 22:08:46 +0000

    Wow
    git-svn-id: svn+ssh://svn.gna.org/svn/clearos@357 f256b4f0-ec0d-4cd5-9998-35f0922a32a5

--- trunk/gui/mandelbrot.c
+++ trunk/gui/mandelbrot.c
@@ -1,4 +1,89 @@
 #include "gui.h"
+#include "kernel/kernel.h"
+#include "drivers/keyboard/keyboard.h"
+
+static int grid[2][200][320];
+static int round=0;
+
+#define by 100
+#define bx 160
+
+static void init(void){
+  grid[0][by][bx]=1;
+  grid[0][by][bx+1]=1;
+  grid[0][by][bx+2]=1;
+  grid[0][by][bx+4]=1;
+  grid[0][by+1][bx]=1;
+  grid[0][by+2][bx+3]=1;
+  grid[0][by+2][bx+4]=1;
+  grid[0][by+3][bx+1]=1;
+  grid[0][by+3][bx+2]=1;
+  grid[0][by+3][bx+4]=1;
+  grid[0][by+4][bx]=1;
+  grid[0][by+4][bx+2]=1;
+  grid[0][by+4][bx+4]=1;
+}
+
+static int G(int a,int b){
+  if(a<0 || b<0 || a==200 || b==320)
+    return 0;
+  return grid[round][a][b];
+}
+
+static void step(void){
+  for(int y=0;y<200;y++)
+    for(int x=0;x<320;x++){
+      int neigh=G(y-1,x-1)+G(y-1,x)+G(y-1,x+1)+
+      G(y,x-1)+G(y,x+1)+
+      G(y+1,x-1)+G(y+1,x)+G(y+1,x+1);
+      int n=G(y,x);
+      if(n){
+        if(neigh!=2 && neigh!=3)
+          n=0;
+      }
+      else if(neigh==3)
+        n=1;
+      grid[1-round][y][x]=n;
+    }
+    round=1-round;
+}
+
+static void print(void){
+  set_cursor(0,0);
+  for(int y=0;y<200;y++)
+    for(int x=0;x<320;x++)
+      VGA_address[VGA_width*y+x]=G(y,x)?1:0;
+}
+
+static void randomize(void){
+  int a=0;
+  static int t=1;
+  t+=2;
+  for(int y=0;y<200;y++)
+    for(int x=0;x<320;x++,a++)
+      grid[round][y][x]=(x+y+a%t)%2;
+}
+
+static void life(void){
+  for(int y=0;y<200;y++)
+    for(int x=0;x<320;x++)
+      grid[0][y][x]=grid[1][y][x]=0;
+  init();
+  int p=0;
+  while(1){
+    print();
+    step();
+    char g;
+    do{
+      //sleep(10);
+      g=getch();
+      if(g=='p')
+        p=1-p;
+    }while(p);
+    if(g=='r')
+      randomize();
+  }
+}
 
 static const int radius=2;
 
@@ -36,4 +121,6 @@ void draw_mandelbrot(void){
           max=count;
       }
     }
+  sleep(2000);
+  life();
 }
