4257f77c64a70551a120681482fa4908bbf0ed1f
morgoth90 <morgoth90@f256b4f0-ec0d-4cd5-9998-35f0922a32a5>
2010-02-16 09:09:04 +0000

    pianificato il supporto elf
    git-svn-id: svn+ssh://svn.gna.org/svn/clearos/trunk@378 f256b4f0-ec0d-4cd5-9998-35f0922a32a5

--- task/task.c
+++ task/task.c
@@ -1,20 +1,6 @@
-/* Copyright (C) 2008 Luca Salmin
- *
- * This file is part of TylyOS.
- *
- * TylyOS is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * TylyOS is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with TylyOS.  If not, see <http://www.gnu.org/licenses/>.
- */
+
+
+
 
 #include "task.h"
 #include <memory/memory.h>
