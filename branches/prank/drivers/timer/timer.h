ff39b32827f824f4af72839445265572600ded86
minibill <minibill@f256b4f0-ec0d-4cd5-9998-35f0922a32a5>
2010-03-29 19:19:30 +0000

    Cleanup #6, x50 commit :D
    git-svn-id: svn+ssh://svn.gna.org/svn/clearos@450 f256b4f0-ec0d-4cd5-9998-35f0922a32a5

--- branches/prank/drivers/timer/timer.h
+++ branches/prank/drivers/timer/timer.h
@@ -1,5 +1,4 @@
-#ifndef TIMER_H_
-#define TIMER_H_
+#pragma once
 
 #define PIT_COMMREG 0x43
 #define PIT_DATAREG0 0x40
@@ -7,9 +6,3 @@
 void initTimer();
 
 void tick(void);
-
-unsigned long int time(void);
-
-void sleep(unsigned int i);
-
-#endif
