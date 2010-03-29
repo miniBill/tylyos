c0a7be0b8744205c38cccaecf2e8c1815d96213f
minibill <minibill@f256b4f0-ec0d-4cd5-9998-35f0922a32a5>
2010-03-29 19:24:07 +0000

    Cleanup #7, moar incoming
    git-svn-id: svn+ssh://svn.gna.org/svn/clearos@451 f256b4f0-ec0d-4cd5-9998-35f0922a32a5

--- branches/prank/memory/memory.c
+++ branches/prank/memory/memory.c
@@ -19,86 +19,31 @@
 
 #include "memory.h"
 #include "gdtflags.h"
-#include <drivers/screen/screen.h>
-#include <lib/string.h>
-#include <kernel/kernel.h>
 
-void memcpy ( char * source, unsigned int count, char * dest )  /*TODO: testare*/
-{
-    if ( (unsigned int)dest < (unsigned int)source ) /*evita di sovrascrivere il buffer durante la copia*/
-    {
-      unsigned int p;
-        for ( p=0;p < count; p++ )
-        {   
-            dest[p]=source[p];
-        }
-    }
-    else
-    {
-      int p;
-        for ( p=count;p>=0; p-- )
-        {
-            dest[count-p]=source[count-p];
-        }
-        
-    }
-   
+unsigned short segmentSelector(unsigned int index,char tableIndicator,char RPL){
+    return ((index<<3)&0xFFF8)|((tableIndicator<<2)&0x4)|RPL;
 }
 
-void invalidateLookasideBuffer()/*costringe la cpu ad aggiornare il lookaside buffer*/
-{
-    write_cr3 ( read_cr3() );
-}
-
-/*ritorna il valore del registro EBP*/
-unsigned int getEBP()
-{
-    unsigned int data=0;
-asm volatile ( "mov %%ebp, %0":"=r" ( data ) );
-    return data;
-}
-/*ritorna il valore del registro ESP*/
-unsigned int getESP()
-{
-    unsigned int data=0;
-asm volatile ( "mov %%esp, %0":"=r" ( data ) );
-    return data;
-}
-
-/*ritorna un selettore di segmento assemblato usando i dati passati*/
-unsigned short segmentSelector ( unsigned int index,char tableIndicator,char RPL )
-{
-    return ( ( index<<3 ) &0xFFF8 ) | ( ( tableIndicator<<2 ) &0x4 ) | ( RPL );
-}
-
-/*setta una riga della GDT (descrittore di segmento)*/
-void gdtSet ( int num, unsigned long base, unsigned long limit, unsigned char gran, unsigned char access )
-{
+void gdtSet(int num, unsigned long base, unsigned long limit, unsigned char gran, unsigned char access){
     gdt[num].baseLow = ( base & 0xFFFF );
     gdt[num].baseMiddle = ( base >> 16 ) & 0xFF;
     gdt[num].baseHigh = ( base >> 24 ) & 0xFF;
 
     gdt[num].limitLow = ( limit & 0xFFFF );
-    /*granularity.low is limit.high*/
     gdt[num].granularity = ( ( limit >> 16 ) & 0x0F );
     gdt[num].granularity |= gran & 0xC0;
     gdt[num].access = access;
 }
 
-/*inizializza i segmenti di default*/
-void initGdt()
-{
+void initGdt(){
     gdtPointer.limit = ( sizeof ( struct gdtEntry ) * NUMERO_SEGMENTI ) - 1;
     gdtPointer.base = ( int ) &gdt;
 
-    gdtSet ( 0, 0, 0, 0, 0 ); /*Il puntatore NULL!*/
-    /*segmento codice kernel*/
+    gdtSet ( 0, 0, 0, 0, 0 );
     gdtSet ( 1, 0, ( 0xFFFFFFFF ) /0x1000,MEM_GRANULAR|MEM_32,
              MEM_PRESENT|MEM_CODE_DATA|MEM_RW|MEM_KERNEL|MEM_CODE );
-    /*segmento dati kernel*/
     gdtSet ( 2, 0, ( 0xFFFFFFFF ) /0x1000,MEM_GRANULAR|MEM_32,
              MEM_PRESENT|MEM_CODE_DATA|MEM_RW|MEM_KERNEL|MEM_DATA );
-
 
     segmentoCodiceKernel=segmentSelector ( 1,0,RPL_KERNEL );
     segmentoDatiKernel=segmentSelector ( 2,0,RPL_KERNEL );
@@ -107,115 +52,3 @@ void initGdt()
 
     gdtFlush ( segmentoCodiceKernel,segmentoDatiKernel );
 }
-
-/*######################### Paginazione ####################################*/
-
-/* ritorna l'indirizzo che indica un selettore di pagetable o pagina */
-inline unsigned int fisicAdressFromSelector ( unsigned int sel )
-{
-    return sel&0xFFFFF000;
-}
-
-unsigned int fisicAdressFromSelectorExt ( unsigned int tableIndex,unsigned int pageIndex )
-{
-    unsigned int pointer=(unsigned int)pageDir+0x1000;
-    unsigned int *obj=( unsigned int* ) ( pointer+(0x1000*tableIndex) + ( pageIndex*4 ) );
-    return fisicAdressFromSelector(*obj);
-}
-
-/* ritorna l'indirizzo logico prendendo come parametri pagetable pagina e offset */
-unsigned int virtualAdress ( unsigned int table,unsigned int page,unsigned int offset )
-{
-    unsigned int temp;
-    temp=offset&0xFFF;
-    temp|= ( page&0x3FF ) <<12;
-    temp|=table<<22;
-    return temp;
-}
-
-/* ritornano le componenti dell indirizzo logico */
-unsigned int getTableFromVirtualAdress ( unsigned int adress )
-{
-    return ( adress>>22 ) &0x3FF;
-}
-unsigned int getPageFromVirtualAdress ( unsigned int adress )
-{
-    return ( adress>>12 ) &0x3FF;
-}
-unsigned int getOffsetFromVirtualAdress ( unsigned int adress )
-{
-    return adress&0xFFF;
-}
-
-/*setta il valore di un selettore*/
-/* pageTableIndex: indice che specifica la page table
- * TableAdress: indirizzo della tabella nella memoria (NB: una tabella occupa una pagina da 4K
- * 
- */
-void setPageTableSelector (unsigned int pageTableIndex ,unsigned int tableAdress,unsigned int flags )
-{
-    unsigned int *obj=( unsigned int* ) ( ( unsigned int ) pageDir + ( pageTableIndex*4 ) );
-    *obj=0;
-    *obj=flags;
-    *obj|= ( tableAdress ) & 0xFFFFF000;
-}
-
-/* pageTableIndex: indice che specifica la page table
- * pageIndex: indice della pagina nella pagetable
- * pageAdress: indirizzo della pagina nella memoria (NB: una pagina occupa 4K
- * 
- */
-void setPageSelector (unsigned int pageTableIndex,unsigned int pageIndex,unsigned int pageAdress,unsigned int flags )
-{
-    unsigned int pointer=(unsigned int)pageDir+0x1000;
-    unsigned int *obj=( unsigned int* ) ( pointer+(0x1000*pageTableIndex) + ( pageIndex*4 ) );
-    *obj=0;
-    *obj=flags;
-    *obj|= ( pageAdress ) & 0xFFFFF000;
-}
-
-
-
-int getBitExt ( unsigned int *bitmap,unsigned int x )
-{
-    int off1,off2;
-    off1=x/32;
-    off2=x%32;
-    return ( bitmap[off1]>>off2 ) &1;
-}
-void setBitExt ( unsigned int *bitmap,unsigned int x,unsigned int value )
-{
-    int off1,off2;
-    off1=x/32;
-    off2=x%32;
-    value&=0x1;
-    if ( value )
-        bitmap[off1]|=1<<off2;
-    else
-        bitmap[off1]&=~ ( 1<<off2 );
-}
-
-/*ritorna l'indice corrispondente ad una pagina fisica da utilizzare nella bitmap delle pagine fisiche*/
-unsigned int convertFisAddrToBitmapIndex ( unsigned int addr )
-{
-    return ( addr-user_start ) /0x1000;
-}
-/*ritorna l'indirizzo fisico corrispondente ad un determinato indice della bitmap delle pagine fisiche*/
-unsigned int convertBitmapIndexToFisAddr ( unsigned int index )
-{
-    return ( index*0x1000 ) +user_start;
-}
-
-/*setta lo stato di una pagina fisica nella bitmap
- * 0 libera
- * 1 allocata
- */
-void setPaginaFisica ( unsigned int indirizzo,unsigned int stato )
-{
-    unsigned int index=convertFisAddrToBitmapIndex(indirizzo);
-    if ( index < mappaPagineFisiche.size )
-    {
-        setBitExt ( mappaPagineFisiche.data,index,stato );
-    }
-}
-
--- branches/prank/memory/memory.h
+++ branches/prank/memory/memory.h
@@ -1,33 +1,7 @@
-/* Copyright (C) 2008 Leonardo Taglialegne <leonardotaglialegne+tylyos@gmail.com>
- * Copyright (C) 2008 Luca Salmin
- *
- * This file is part of TylyOS.
- *
- * TylyOS is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * TylyOS is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with TylyOS.  If not, see <http://www.gnu.org/licenses/>.
- */
-#ifndef MEMORY_H_
-#define MEMORY_H_
+#pragma once
 
-unsigned int getEBP();
-unsigned int getESP();
-
-/*setta una riga della GDT (descrittore di segmento)*/
 void gdtSet ( int num, unsigned long base, unsigned long limit, unsigned char gran, unsigned char access );
-
-/*ritorna un selettore di segmento assemblato usando i dati passati*/
 unsigned short segmentSelector ( unsigned int index,char tableIndicator,char RPL );
-
 void initGdt();
 
 struct gdtEntry
@@ -55,7 +29,6 @@ unsigned short segmentoCodiceKernel,segmentoDatiKernel;/*selettori di segmento k
 unsigned short segmentoCodiceUser,segmentoDatiUser;/*selettori di segmento user*/
 
 extern void gdtFlush ( unsigned short selettoreSegmentoCodice,unsigned short selettoreSegmentoDati );
-
 
 #define PAG_PRESENT 	0x1
 #define PAG_NOTPRESENT 	0x0
@@ -86,69 +59,3 @@ unsigned int memoriaFisica; /* byte di memoria fisica */
 extern unsigned int l_pageDir,l_end,l_tempPage;
 unsigned int *pageDir; /* area da 4096byte che ospita la pagedir del kernel seguita dall' area per le page tables*/
 char *tempPage;
-
-void initPaging();
-
-void setPageTableSelector (unsigned int pageTableIndex ,unsigned int tableAdress,unsigned int flags );
-void setPageSelector (unsigned int pageTableIndex,unsigned int pageIndex,unsigned int pageAdress,unsigned int flags );
-
-int getBitExt ( unsigned int *bitmap,unsigned int x );
-void setBitExt ( unsigned int *bitmap,unsigned int x,unsigned int value );
-
-
-/* ritornano le componenti dell indirizzo logico */
-unsigned int getTableFromVirtualAdress ( unsigned int adress );
-unsigned int getPageFromVirtualAdress ( unsigned int adress );
-unsigned int getOffsetFromVirtualAdress ( unsigned int adress );
-
-/* ritorna l'indirizzo logico prendendo come parametri pagetable pagina e offset */
-unsigned int virtualAdress ( unsigned int table,unsigned int page,unsigned int offset );
-
-/* ritorna l'indirizzo che indica un selettore di pagetable o pagina */
-unsigned int fisicAdressFromSelector ( unsigned int sel );
-unsigned int fisicAdressFromSelectorExt ( unsigned int tableIndex,unsigned int pageIndex );
-
-extern unsigned int read_cr0();
-extern void write_cr0 ( unsigned int data );
-extern unsigned int read_cr3();
-extern void write_cr3 ( unsigned int data );
-
-
-
-/*descrittore di pagina fisica*/
-struct pagina
-{
-    unsigned int procID;
-    unsigned int indirizzoLog;   /*indirizzo logino alla quale e' mappata la pagina nel processo*/
-    unsigned int indirizzoFis;    /*indirizzo fisico, se uguale a 0 e' swappata sull hd */
-    struct pagina *next;
-};
-
-struct bitmap
-{
-    unsigned int *data;
-    unsigned int size;
-};
-
-/*rappresenta attraverso una bitmap lo stato di ogni pagina fisica in memoria
- * 0 non usata
- * 1 usata
- *
- * NB: le pagine fisiche a cui si riferisce la bitmap sono quelle mappate dopo il kernel e vengono usate sia per i processi sia per l'heap del kernel stesso
- */
-struct bitmap mappaPagineFisiche;
-
-char mappaPagineFisicheBitmapData[(1024*1024)/8];/*un bit per ogni pagina del sistema, stimato per eccesso in modo di garantire 4GB di ram fisica*/
-
-/*ritorna l'indice corrispondente ad una pagina fisica da utilizzare nella bitmap delle pagine fisiche*/
-unsigned int convertFisAddrToBitmapIndex ( unsigned int addr );
-/*ritorna l'indirizzo fisico corrispondente ad un determinato indice della bitmap delle pagine fisiche*/
-unsigned int convertBitmapIndexToFisAddr ( unsigned int index );
-
-/*setta lo stato di una pagina fisica nella bitmap
- * 0 libera
- * 1 allocata
- */
-void setPaginaFisica ( unsigned int indirizzo,unsigned int stato );
-
-#endif
