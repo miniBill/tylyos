a4f7da148ae9a6d7f98b4e763b921008c4a17447
morgoth90 <morgoth90@f256b4f0-ec0d-4cd5-9998-35f0922a32a5>
2010-05-21 09:52:46 +0000

    scheduler incoming
    git-svn-id: svn+ssh://svn.gna.org/svn/clearos@483 f256b4f0-ec0d-4cd5-9998-35f0922a32a5

---
+++ trunk/task/scheduler.c
@@ -0,0 +1,49 @@
+/* Copyright (C) 2008 Luca Salmin
+*
+* This file is part of TylyOS.
+*
+* TylyOS is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* TylyOS is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with TylyOS.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "scheduler.h"
+#include <task/dispatcher.h>
+
+void initScheduler()
+{
+    scheduleTimeSlice=500;
+}
+
+/*setta ogni quanti millisecondi viene eseguito l' algoritmo di scheduling*/
+void setSchedulerTimeSlice(unsigned int ms)
+{
+    scheduleTimeSlice=ms;
+}
+
+/*abilita lo scheduler*/
+void startScheduler()
+{
+}
+
+/*disabilita lo scheduling dei task*/
+void stopScheduler()
+{
+}
+
+/*funzione che sceglie il task da mandare in esecuzione e che lo manda in esecuzione usando il dispatcher*/
+void schedule();
+/*
+funzioneche viene richimata quando nessun task ha niente da fare
+dovra' mandare in esecuzione un task dedicato alla gestione dei tempi morti
+*/
+void idle();
